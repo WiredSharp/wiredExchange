ca513b4efea946253a6e345e2e051259ab1fc80a
WiredSharp <zericco@gmail.com>
2021-12-06 21:44:12 +0100

    feat: allow refresh transactions

--- streamlit/wallet.py
+++ streamlit/wallet.py
@@ -4,8 +4,7 @@ from logging.config import fileConfig
 from dotenv import load_dotenv
 from wired_exchange.portfolio import Portfolio
 import streamlit as st
-import pandas as pd
-import numpy as np
+
 import glob
 
 logging.config.fileConfig('logging.conf')
@@ -19,11 +18,20 @@ def foreground_by_sign(v):
     return 'color: red' if v < 0 else 'color: green' if v > 0 else ""
 
 
+def import_all(wallet: Portfolio):
+    wallet.import_account_operations()
+    wallet.import_transactions()
+
+
 logger = logging.getLogger('main')
 logger.info('--------------------- starting Wired Exchange ---------------------')
 profile = st.selectbox("select portfolio", get_profiles())
+
 load_dotenv(f'.env-{profile}')
 wallet = Portfolio(profile)
+if st.button('Update Wallet'):
+    import_all(wallet)
+
 st.title(f'{profile} Wallet')
 summary = wallet.get_summary()
 st.dataframe(summary[['total', 'available', 'PnL_pc', 'average_buy_price', 'price',
